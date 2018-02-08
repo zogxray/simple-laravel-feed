8926086abcfde926c667ee2266e6318ee47d03a8
Cyril <cyril@acid.fr>
2018-02-08 11:32:07 +0100

    Update views to accept accent in author name and titles

--- src/views/atom.blade.php
+++ src/views/atom.blade.php
@@ -1,33 +1,33 @@
 {!! '<'.'?'.'xml version="1.0" encoding="UTF-8" ?>' !!}
 <feed xmlns="http://www.w3.org/2005/Atom">
-    <title type="text">{{$channel->title}}</title>
+    <title type="text">{!! $channel->title !!}</title>
     <subtitle type="html"><![CDATA[{!!$channel->description!!}]]></subtitle>
     <link href="{{$channel->link}}"></link>
     <id>{{$channel->link}}</id>
-    <link rel="alternate" type="text/html" href="{{$channel->link}}" ></link>
-    <link rel="self" type="application/atom+xml" href="{{$channel->link}}" ></link>
-@if (!empty($channel->logo))
+    <link rel="alternate" type="text/html" href="{{$channel->link}}"></link>
+    <link rel="self" type="application/atom+xml" href="{{$channel->link}}"></link>
+    @if (!empty($channel->logo))
         <logo>{{$channel->logo}}</logo>
-@endif
-@if (!empty($channel->icon))
+    @endif
+    @if (!empty($channel->icon))
         <icon>{{$channel->icon}}</icon>
-@endif
+    @endif
     <updated>{{$channel->pubdate}}</updated>
-@foreach($items as $item)
+    @foreach($items as $item)
         <entry>
             <author>
-                <name>{{$item->author['name']}}</name>
+                <name>{!! $item->author['name'] !!}</name>
                 <uri>{{$item->author['url']}}</uri>
                 <email>{{$item->author['email']}}</email>
             </author>
-            <title type="text">{{$item->title}}</title>
+            <title type="text">{!! $item->title !!}</title>
             <link rel="alternate" type="text/html" href="{{$item->link}}"></link>
             <id>{{$item->link}}</id>
             <summary type="html"><![CDATA[{!!$item->description!!}]]></summary>
-@if (!empty($item->content))
-            <content type="html"><![CDATA[{!!$item->content!!}]]></content>
-@endif
+            @if (!empty($item->content))
+                <content type="html"><![CDATA[{!!$item->content!!}]]></content>
+            @endif
             <updated>{{$item->pubdate}}</updated>
         </entry>
-@endforeach
+    @endforeach
 </feed>
